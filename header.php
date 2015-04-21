fd6747d31619994525ee3f1064e53c1429313ce2
marvinzamoras <marvinzamoras@gmail.com>
2015-04-21 14:33:38 +0800

    first commit

---
+++ header.php
@@ -0,0 +1,32 @@
+	<!-- start header -->
+	<header id=header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container container1">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="img/logo1.png" alt="logo" /></a>
+                </div>
+			</div>
+			<div class="container container2">
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Home</a></li> 
+						<li><a href="about.php">About Us</a></li>
+						<li><a href="services.html">Programs and Services</a></li>
+                        <li><a href="portfolio.html">Special Projects</a></li>
+                        <li><a href="pricing.html">News and Updates</a></li>
+						<li><a href="pricing.html">Article</a></li>
+						<li><a href="pricing.html">Gallery</a></li>
+						<li><a href="pricing.html">Magazine</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+			</div>
+            
+        </div>
+	</header>
+	<!-- end header -->
